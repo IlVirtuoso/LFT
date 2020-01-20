368a6521dc57e94401298e618195faf8299a17a2
ImSans <matteo.ielacqua@edu.unito.it>
2020-01-20 07:41:12 +0100

    u

--- Generic/Exercise.java
+++ Generic/Exercise.java
@@ -13,25 +13,33 @@ public class Exercise {
         int automa = scan.nextInt();
         String query;
         switch (automa) {
-        case '0':
+        case 0:
             System.out.println("Digit a comment");
-            query = scan.nextLine();
-            if (CommentParse(q))
+            query = scan.next();
+            if (CommentParse(query))
                 System.out.print("Success");
             else
                 System.out.print("Is not A comment");
             break;
 
-        case '1':
+        case 1:
             System.out.println("Digit an identifier");
-            query = scan.nextLine();
-            if (IDParse(q))
+            query = scan.next();
+            if (IDParse(query))
                 System.out.println("This id is correct");
             else
                 System.out.print("This id isn't correct");
             break;
 
+        case 2:
+            System.out.println("Digit a Student");
+            query = scan.next();
+            if (StudentParse(query))
+                System.out.println("You are of turn T2 or T3");
+            else
+                System.out.println("Sorry you are not in either of those turns");
         }
+        scan.close();
     }
 
     public static boolean CommentParse(String q) {
@@ -116,16 +124,46 @@ public class Exercise {
         return state == 2;
     }
 
-    public static boolean StudentParse(String query){
+    public static Boolean StudentParse(String q) {
         int i = 0;
         int state = 0;
         while (state >= 0 && i < q.length()) {
             char ch = q.toLowerCase().charAt(i++);
             switch (state) {
-                
+            case 0:
+                if (ch >= 'a' && ch <= 'z')
+                    state = -1;
+                else if (Integer.parseInt(String.valueOf(ch)) % 2 == 0)
+                    state = 2;
+                else if (Integer.parseInt(String.valueOf(ch)) % 2 != 0)
+                    state = 1;
+
+                break;
+
+            case '2':
+                if (ch >= 'a' && ch <= 'k')
+                    state = 3;
+                else if (ch >= 'l' && ch <= 'z')
+                    state = -1;
+                else if (Integer.parseInt(String.valueOf(ch)) % 2 == 0)
+                    state = 2;
+                else if (Integer.parseInt(String.valueOf(ch)) % 2 != 0)
+                    state = 0;
+                break;
+
+            case '1':
+                if (ch >= 'a' && ch <= 'k')
+                    state = -1;
+                else if (ch >= 'l' && ch <= 'z')
+                    state = 3;
+                else if (Integer.parseInt(String.valueOf(ch)) % 2 != 0)
+                    state = 1;
+                else if (Integer.parseInt(String.valueOf(ch)) % 2 == 0)
+                    state = 0;
+
             }
         }
-        return state == 0;
+        return state == 3;
     }
 
 }
